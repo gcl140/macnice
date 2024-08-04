e1d21a3f9bc771c2ad1d5bb48fce6567a13aeb05
gcl140 <140011591+gcl140@users.noreply.github.com>
2024-08-04 06:14:24 +0300

    Update script_order.js

--- scripts/script_order.js
+++ scripts/script_order.js
@@ -218,3 +218,19 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     }
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 100) {
+            backToTopButton.classList.add('show');
+        } else {
+            backToTopButton.classList.remove('show');
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
